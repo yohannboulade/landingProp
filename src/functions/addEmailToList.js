926aaaac8d2c02faa9f0c5cc1908fe0723a8e92d
Yohann Boulade <yohann.boulade@gmail.com>
2023-09-16 17:20:01 +0200

    t8

--- src/functions/addEmailToList.js
+++ src/functions/addEmailToList.js
@@ -1,11 +1,14 @@
 import fetch from 'node-fetch';
 
 exports.handler = async (event, context) => {
+  // Récupère les données d'email et listId à partir du corps de la requête
   const { email, listId } = JSON.parse(event.body);
 
+  // Vos clés d'API Mailjet
   const apiKey = '3d5841c70007a337011a74cfacbcc4c2';
   const apiSecret = 'e6d8db6a84f03ba81e1f27eba486a63a';
 
+  // Construit l'URL de l'API Mailjet pour ajouter l'email à la liste
   const apiUrl = `https://api.mailjet.com/v3/REST/contact/${email}`;
   const data = {
     IsExcludedFromCampaigns: 'false',
@@ -13,6 +16,7 @@ exports.handler = async (event, context) => {
   };
 
   try {
+    // Effectue la requête POST à l'API Mailjet
     const response = await fetch(apiUrl, {
       method: 'POST',
       headers: {
@@ -22,6 +26,7 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(data),
     });
 
+    // Gère la réponse de l'API Mailjet
     if (!response.ok) {
       const errorMessage = await response.text();
       return {
@@ -30,11 +35,13 @@ exports.handler = async (event, context) => {
       };
     }
 
+    // Si la requête s'est bien passée
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Adresse email ajoutée avec succès.' }),
     };
   } catch (error) {
+    // Gère les erreurs de la requête
     return {
       statusCode: 500,
       body: JSON.stringify({ message: `Erreur serveur : ${error.message}` }),
